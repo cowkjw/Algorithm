852de840b49afe14fe31dcadcded701a192c559b
cowkjw <cowkjw@naver.com>
2021-12-14 02:24:25 +0900

    Update 2562.cpp
    code change

--- BakeJoon/2562.cpp
+++ BakeJoon/2562.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main()
 {
 
-	int numbers[10] = {};
-	int Max = numbers[0];
+	int num;
+	int Max = 0;
 	int index;
 
 	for (int i = 0; i < 9; i++)
 	{
-		cin >> numbers[i];
-		if (Max < numbers[i])
+		cin >> num;
+		if (Max < num)
 		{
-			Max = numbers[i];
+			Max = num;
 			index = i;
 		}
 	}
 
-	cout << Max << "\n" << index+1;
+	cout << Max << "\n" << index + 1;
 
 
 	return 0;
