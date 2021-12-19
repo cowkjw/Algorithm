d4f5657c4804d2e4d9578c86d25f3b6674c1377f
cowkjw <cowkjw@naver.com>
2021-12-19 21:36:47 +0900

    3986번
    노가다 풀이와 개선 된 풀이

--- BakeJoon/3986.cpp
+++ BakeJoon/3986.cpp
@@ -8,10 +8,48 @@ using namespace std;
 int main()
 {
 
-	string AB;
+
+
+	//string AB;
+	//int N, j;
+	//int cnt = 0;
+
+	//cin >> N;
+
+	//for (int i = 0; i < N; i++)
+	//{
+	//	cin >> AB;
+	//	for (j = 0; j < AB.size();)
+	//	{
+	//		if (AB[j] == AB[j + 1])
+	//		{
+	//			AB.erase(j, 2);
+	//			j = 0;
+	//		}
+	//		else
+	//		{
+	//			j++;
+
+	//		}
+
+	//	}
+	//	if (AB.empty())
+	//	{
+	//		cnt++;
+	//	}
+	//}
+	//cout << cnt;
+
+
+
+
+
+
+
 	int N, cnt = 0;
 
 	cin >> N;
+	string AB;
 
 	while (N--)
 	{
