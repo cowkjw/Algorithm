b0e9d3d6b67e22ac976a7fcd51066285ab7a3075
cowkjw <cowkjw@naver.com>
2022-01-14 00:30:26 +0900

    3273번

---
+++ BakeJoon/3273.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[2000001] = {};
+int N[100001] = {};
+
+int main()
+{
+
+
+	int size, X, cnt = 0;
+
+	cin >> size;
+
+
+
+
+	for (int i = 0; i < size; i++)
+	{
+		cin >> N[i];
+	}
+
+	cin >> X;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[X - N[i]] > 0&&X-N[i]>0) // 수 판단을 넣어줘야한다
+			cnt++;
+
+		arr[N[i]] ++;
+	}
+
+	cout << cnt;
+	return 0;
+}
