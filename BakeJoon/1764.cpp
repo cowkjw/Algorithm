32260bf9023abd6c79c308f8c37fd6d26ca7abab
cowkjw <cowkjw@naver.com>
2022-01-20 23:24:23 +0900

    1764번

---
+++ BakeJoon/1764.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	vector<string> people;
+
+	int N; // 듣도 못한 사람 수
+	int M; // 보도 못한 사람의 수
+	int cnt = 0;
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N + M; i++)
+	{
+		string person;
+
+		cin >> person;
+		people.push_back(person);
+	}
+
+	sort(people.begin(), people.end());
+
+	vector<string> check;
+	for (int i = 0; i < people.size() - 1; i++)
+	{
+		if (people[i] == people[i + 1])
+		{
+			cnt++;
+			check.push_back(people[i]);
+		}
+	}
+	cout << cnt << "\n";
+	for (auto it = check.begin(); it != check.end(); ++it)
+		cout << (*it) << "\n";
+
+}
+
