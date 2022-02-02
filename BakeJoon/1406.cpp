f08e084d024aa680220531598216ec53502f1b91
cowkjw <cowkjw@naver.com>
2022-02-02 23:01:51 +0900

    1406번

---
+++ BakeJoon/1406.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string N;
+	list<char> L;
+	int M;
+
+	cin >> N;
+
+	for (auto i : N)
+		L.push_back(i);
+
+	auto cursorPos = L.end();
+
+	cin >> M;
+
+	while (M--)
+	{
+		char com;
+		cin >> com;
+
+		if (com == 'P')
+		{
+			char add;
+			cin >> add;
+			L.insert(cursorPos, add);
+		}
+		else if (com == 'L')
+		{
+			if (cursorPos != L.begin())
+				cursorPos--;
+		}
+		else if (com == 'D')
+		{
+			if (cursorPos != L.end())
+				cursorPos++;
+		}
+		else if (com == 'B')
+		{
+			if (cursorPos != L.begin())
+			{
+				cursorPos--;
+				cursorPos = L.erase(cursorPos); // 삭제하고 다음 원소를 가리키는 반복자 반환
+			}
+		}
+
+	}
+	for (auto i : L)
+		cout << i;
+
+}
