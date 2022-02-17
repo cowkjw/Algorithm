6aff698e6cbb9ae84abffdd5fd348b3064d620ce
cowkjw <cowkjw@naver.com>
2022-02-18 00:54:40 +0900

    로또의 최고 순위와 최저 순위
    레벨 1

---
+++ Programmers/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+
+    int isZero = 0;
+    int winners = 0;
+
+    for (int i = 0; i < lottos.size(); i++)
+    {
+        if (lottos[i] == 0)
+        {
+            isZero++;
+            continue;
+        }
+        else
+        {
+            for (int j = 0; j < win_nums.size(); j++)
+            {
+                if (lottos[i] == win_nums[j])
+                {
+                    winners++;
+                }
+            }
+        }
+
+    }
+
+
+    if (winners == 0 && isZero > 0)
+    {
+        answer.push_back(7 - (winners + isZero));
+        answer.push_back(6);
+    }
+    else if (isZero == 0 && winners == 0)
+    {
+        answer.push_back(6);
+        answer.push_back(6);
+    }
+    else
+    {
+        answer.push_back(7 - (winners + isZero));
+        answer.push_back(7 - winners);
+    }
+
+    return answer;
+}
