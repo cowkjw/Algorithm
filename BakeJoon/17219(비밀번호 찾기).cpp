48ad9d0ea7f29cb45235838fb18a1549d1f35c9e
cowkjw <cowkjw@naver.com>
2022-07-04 10:21:22 +0900

    Update 17219(비밀번호 찾기).cpp

--- BakeJoon/17219(비밀번호 찾기).cpp
+++ BakeJoon/17219(비밀번호 찾기).cpp
@@ -6,21 +6,16 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	map<string, string> m;
+	unordered_map<string, string> m;
 	int N, M;
 
 	cin >> N >> M;
-	cin.ignore();
 
 	for (int i = 0; i < N; i++)
 	{
-		string input;
-		string site, password;
-		getline(cin, input);
-		int idx = input.find(" ");
 
-		site = input.substr(0, idx);
-		password = input.substr(idx+1);
+		string site, password;
+		cin >> site >> password;
 
 		m[site] = password;
 
