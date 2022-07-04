3f2c4122f26be6eaa99473b64b86e944f37ce6c5
cowkjw <cowkjw@naver.com>
2022-07-04 23:32:10 +0900

    1026(보물)
    백준 실버4

---
+++ BakeJoon/1026(보물).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<int> A(N);
+	vector<int> B(N);
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+	for (int i = 0; i < N; i++)
+		cin >> B[i];
+
+	sort(A.begin(), A.end());
+	sort(B.rbegin(), B.rend());
+	int sum = 0;
+	for (int i = 0; i < N; i++)
+		sum += A[i] * B[i];
+
+	cout << sum;
+
+	return 0;
+
+}
+
