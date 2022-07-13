d8408233773e30835cf9e235a1d0d52f3decea46
cowkjw <cowkjw@naver.com>
2022-07-13 12:23:40 +0900

    2178(미로탐색)
    백준 실버1

---
+++ BakeJoon/2178(미로탐색).cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+string board[102];
+int dist[102][102];
+int n, m;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> board[i];
+	for (int i = 0; i < n; i++)
+		fill(dist[i], dist[i] + m, -1);
+
+	queue<pair<int, int>> q;
+	q.push({ 0,0 });
+	dist[0][0] = 0;
+
+	while (!q.empty())
+	{
+
+		auto cur = q.front();
+		q.pop();
+
+		for (int dir = 0; dir < 4; dir++) //»óÇÏÁÂ¿ì Ã¼Å©
+		{
+			int nx = cur.first + dx[dir]; // xÁÂÇ¥
+			int ny = cur.second + dy[dir]; // yÁÂÇ¥
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+			if (dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
+			dist[nx][ny] = dist[cur.first][cur.second] + 1;
+			q.push({ nx,ny });
+		}
+	}
+
+	cout << dist[n - 1][m - 1] + 1;
+}
