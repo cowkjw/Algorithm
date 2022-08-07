f41103ce327c605175ef95e33bd1684096ba8df6
cowkjw <cowkjw@naver.com>
2022-08-07 20:39:02 +0900

    11726(2xn 타일링)
    백준 실버 3

---
+++ BakeJoon/11726(2×n 타일링).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int dp[1001];
+int n;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+
+
+	dp[1] = 1; // 1일때 경우의 수
+	dp[2] = 2; // 2일때 경우의 수
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007; // i-1일때 1가지 i-2일때 1가지 
+	}
+	cout << dp[n];
+
+
+	return 0;
+
+
+}
