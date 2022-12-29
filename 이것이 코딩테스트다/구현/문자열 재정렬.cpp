0ed488c5be418b9ef041f0214f3017aa1fd38a53
cowkjw <cowkjw@naver.com>
2022-12-29 22:30:57 +0900

    문자열 재정렬
    챕터12 구현

---
+++ 이것이 코딩테스트다/구현/문자열 재정렬.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+#define INF 987654321
+
+using namespace std;
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string str;
+	cin >> str;
+
+	string ans;
+	int num = 0;
+
+	for (auto const& c : str)
+	{
+		if (isdigit(c))
+		{
+			num += c - '0';
+		}
+		else
+		{
+			ans += c;
+		}
+	}
+
+	sort(ans.begin(), ans.end());
+	ans += to_string(num);
+
+	cout << ans;
+
+	return 0;
+}
+
+
+
+
+
+
+
