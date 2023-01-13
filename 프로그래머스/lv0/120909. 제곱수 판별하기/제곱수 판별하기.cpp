0e889fb4c8c2d7c2ad3588041826923f87848003
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-13 17:35:33 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -6,11 +6,6 @@ using namespace std;
 
 int solution(int n) {
     
-    for(int i = 1;i<=n/2;i++)
-    {
-        if(pow(i,2)==n)
-            return 1;
-    }
-    
-    return 2;
+  int i = sqrt(n);
+    return i * i == n ? 1 : 2;
 }
