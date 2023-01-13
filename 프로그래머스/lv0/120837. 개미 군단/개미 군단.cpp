c1c93b60831ba00634667dd969498b8f2e0f32d1
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-13 16:56:41 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    
+    while(hp>0)
+    {
+        if(hp-5>=0)
+        {
+            answer++;
+            hp-=5;
+        }
+        else if(hp-3>=0)
+        {
+            answer++;
+            hp-=3;
+        }
+        else
+        {
+            answer++;
+            hp--;
+        }
+    }
+    
+    return answer;
+}
