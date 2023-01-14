fc2ecfbbc59be684b9ff1892ed2afbff6ccc668f
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-14 15:46:30 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 4.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+string solution(string bin1, string bin2) {
+	string answer = "";
+	bitset<10>bit1(bin1);
+	bitset<10>bit2(bin2);
+	int n1 = bit1.to_ulong();	// type = unsigned long integer
+	int n2 = bit2.to_ulong();	// type = unsigned long integer
+	int num = n1 + n2;
+	bitset<11>bit3(num);
+	answer = bit3.to_string();
+    for(int i = 0;i<answer.size();i++)
+    {
+        if(answer[i]=='0'&&answer[i+1]=='1')
+        {
+            answer = answer.substr(i+1);
+            break;
+        }
+    }
+	if(answer[0]=='0')
+        return "0";
+	return answer;
+}
