5cd7b394356c819a049d36536f29239625200ab0
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-14 17:07:56 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int solution(vector<int> numbers) {
 
-    int answer = -100000001;
+    int answer = -100000000;
     for(int i = 0;i<numbers.size()-1;i++)
     {
         for(int j = i+1;j<numbers.size();j++)
