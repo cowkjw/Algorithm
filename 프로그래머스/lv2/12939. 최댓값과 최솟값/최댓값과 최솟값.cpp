3c8bc4e3d0236906f2da6c90c33754a46387f28c
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-16 16:00:51 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> split(string str, string delimiter)
+{
+	vector<string> ret;
+	long long pos = 0;
+	string token;
+	while ((pos = str.find(delimiter)) != string::npos)
+	{
+		token = str.substr(0, pos);
+		ret.push_back(token);
+		str.erase(0, pos + delimiter.length());
+	}
+	ret.push_back(str);
+	return ret;
+}
+
+
+string solution(string s) {
+	string answer = "";
+	vector<string> splitStr = split(s, " ");
+
+	vector<int> v;
+    
+	transform(splitStr.begin(), splitStr.end(), back_inserter(v), [](string str)
+		{
+			return stoi(str);
+		});
+    
+	answer += to_string(*min_element(v.begin(), v.end()))+ " " + to_string(*max_element(v.begin(), v.end()));
+	return answer;
+}
