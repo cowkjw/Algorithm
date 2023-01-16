885c35b0138a315f54718a4498414070c804018a
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-17 01:59:04 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+
+	for (int i = 1; i <= n; i++)
+	{
+		
+		answer++;
+		while (answer % 3 == 0 || to_string(answer).find("3") != -1)
+		{
+			answer++;
+		}
+	}
+	return answer;
+}
