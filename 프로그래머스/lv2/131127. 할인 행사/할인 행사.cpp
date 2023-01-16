c1ff0e2604b544fb3642d0510e94ff7a47d7b9d7
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-16 13:42:24 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    int answer = 0;
+    unordered_map<string,int> m;
+    
+    for(int i = 0;i<want.size();i++)
+    {
+        m[want[i]] = number[i];
+    }
+    
+    int size = discount.size()-9; // 인덱스 범위 생각
+    
+    for(int i = 0;i<size;i++)
+    {
+        unordered_map<string,int> temp = m; // 복사
+        
+        for(int j = i;j<10+i;j++)
+        {
+            if(temp.find(discount[j])!=temp.end())
+            {
+                temp[discount[j]]--;
+            }
+               
+            if(temp[discount[j]]==0)
+            {
+                temp.erase(discount[j]);
+            }       
+            
+            if(temp.empty())
+            {
+                answer++;
+            }
+               
+        }
+    
+    }
+    
+    
+    return answer;
+}
