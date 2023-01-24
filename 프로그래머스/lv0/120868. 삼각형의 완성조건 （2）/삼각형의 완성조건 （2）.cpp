d9025eab56b3b0140565b24160d17f4fdc96541b
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-25 00:11:58 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
@@ -9,14 +9,6 @@ int solution(vector<int> sides) {
     auto minNum = *min_element(sides.begin(), sides.end());
     auto maxNum = *max_element(sides.begin(), sides.end());
  
-    for(int i = maxNum-minNum ;i<=maxNum;i++)
-    {
-        answer++;
-    }
-    for(int i = maxNum;i<maxNum+minNum;i++)
-    {
-        answer++;
-    }
     
-    return answer-2;
+    return (minNum+maxNum) - (maxNum-minNum) -1;
 }
