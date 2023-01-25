9627811971049553d186960f6e29a2e8f547de30
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-25 17:03:01 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int M, int N) {
+    
+    return M*N-1; //  n-1 + n(m-1)  = nm - 1
+                 // m-1 + m(n-1) = nm-1
+}
