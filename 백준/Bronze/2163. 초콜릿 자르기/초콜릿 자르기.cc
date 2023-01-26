439b8f3737d92f77c86e451be0695ee37fdc6fbb
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-01-26 18:15:28 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define X first 
+#define Y second
+
+using namespace std;
+
+int n, m;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n>>m;
+
+	cout << n*m-1;
+	return 0;
+}
