73920cf047df7150c7254affde622ff38b8ae2ca
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-02-01 03:09:56 +0900

    [Silver III] Title: 예산, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.cc
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+#define INF 987654321
+#define X first 
+#define Y second
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	int budget;
+	cin >> budget;
+	int start = 0;
+	int end = v.back();
+	int ans = 0;
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+		int cnt = 0;
+		int tempBudget = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (v[i] <= mid)
+			{
+				tempBudget += v[i];
+			}
+			else
+			{
+				tempBudget += mid;
+			}
+		}
+		if (tempBudget > budget)
+		{
+			end = mid - 1;
+		}
+		else
+		{
+			ans = mid;
+			start = mid + 1;
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
