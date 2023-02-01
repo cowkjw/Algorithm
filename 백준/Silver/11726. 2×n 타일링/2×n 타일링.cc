994d5c932e22b6c17219c1c10e3669da18b443ad
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-02-01 18:05:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-
-
-
 int dp[1001];
-int n;
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	int n;
 	cin >> n;
-	
 
 	dp[1] = 1;
 	dp[2] = 2;
 
 	for (int i = 3; i <= n; i++)
 	{
-		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
 	}
 	cout << dp[n];
-
-
 	return 0;
-
-
 }
