29ac900fcf342005f5cff9d81a4588e8d912a0e0
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-02-03 14:09:39 +0900

    [Silver II] Title: 좌표 압축, Time: 464 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define X first 
+#define Y second
 
+
+using namespace std;
 
 int main()
 {
@@ -23,28 +28,7 @@ int main()
 
 	for (int i = 0; i < n; i++)
 	{
-		int start = 0;
-		int end = v.size() - 1;
-		int ans = 0;
-		while (start <= end)
-		{
-			int mid = (start + end) / 2;
-			if (v[mid] < temp[i])
-			{
-				start = mid + 1;
-			}
-			else if (v[mid] > temp[i])
-			{
-				end = mid - 1;
-			}
-			else
-			{
-				ans = mid;
-				break;
-			}
-
-		}
-		cout << ans << " ";
+		cout << lower_bound(v.begin(), v.end(), temp[i]) - v.begin()<<" ";
 	}
 
 	return 0;
