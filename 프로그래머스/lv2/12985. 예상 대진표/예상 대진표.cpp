8de04081db34cd8f7eb8ad02b517ed93aefa5751
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-02-21 20:24:01 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    
+    while(abs(a-b)!=0)
+    {
+       
+        answer++;
+        cout<<a<<endl;
+        a=ceil(float(a)/2);
+        b=ceil(float(b)/2);
+    }
+
+
+    return answer;
+}
