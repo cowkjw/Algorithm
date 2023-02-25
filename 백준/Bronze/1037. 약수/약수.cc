53f3def75d821f7697fd89ac231d06ace2a037c0
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-02-25 18:41:34 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int N, A;
+	int Min = 1000000, Max = 2;
+
+	cin >> N;
+
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A;
+
+		Min > A ? Min = A : Min = Min;
+		A > Max ? Max = A : Max = Max;
+	}
+
+	if (N == 1 && A == 2)
+	{
+		cout << A * 2;
+	}
+	else
+		cout << Min * Max;
+
+
+
+	return 0;
+}
+
+
