78e8cfc69f34635b4cda6377f6efdd3ccd618a70
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-04-20 19:31:52 +0900

    [unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
@@ -5,17 +5,10 @@ using namespace std;
 
 string solution(string str1, string str2) {
     string answer = "";
-    int i= 0,j = 0;
-    for(int n = 0;n<str1.size()+str2.size();n++)
+    for(int n = 0;n<str1.size();n++)
     {
-        if(n%2==0)
-        {
-            answer+=str1[i++];
-        }
-        else
-        {
-            answer+=str2[j++];
-        }
+      answer.push_back(str1[n]);
+        answer.push_back(str2[n]);
     }
     return answer;
 }
