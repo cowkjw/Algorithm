0812743a28378703482553def43a2f5ef41936d2
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-04-24 01:12:10 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.22 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(string myString, string pat) {
+    transform(myString.begin(),myString.end(),myString.begin(),::tolower);
+    transform(pat.begin(),pat.end(),pat.begin(),::tolower);
+    
+    return myString.find(pat)!=-1;
+}
