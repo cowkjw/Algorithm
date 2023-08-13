2373e86b62f54d1e5d916149089f5c3f4a50b71e
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-08-13 16:52:38 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -7,19 +7,6 @@
 
 using namespace std;
 
-vector<string> split(string input, string delimiter)
-{
-	vector<string> ret;
-	long long pos = 0;
-	string token = "";
-	while ((pos = input.find(delimiter)) != string::npos) {
-		token = input.substr(0, pos);
-		ret.push_back(token);
-		input.erase(0, pos + delimiter.length());
-	}
-	ret.push_back(input);
-	return ret;
-}
 
 int main()
 {
@@ -30,18 +17,21 @@ int main()
 	cin >> n;
 	string pattern;
 	cin >> pattern;
-	vector<string> v = split(pattern, "*");
+	string pre, suf;
+	int pos = pattern.find("*");
+	pre = pattern.substr(0, pos);
+	suf = pattern.substr(pos+1);
 	
 	while (n--)
 	{
 		string str;
 		cin >> str;
 		
-		if(v[0].size()+v[1].size()>str.size()) cout<<"NE\n";
+		if(pre.size()+ suf.size()>str.size()) cout<<"NE\n";
 		else
 		{
 
-		if (v[0]==str.substr(0,v[0].size())&&v[1]==str.substr(str.size()-v[1].size())) cout << "DA\n";
+		if (pre ==str.substr(0, pre.size())&&suf==str.substr(str.size()-suf.size())) cout << "DA\n";
 		else cout << "NE\n";
 		}
 	}
