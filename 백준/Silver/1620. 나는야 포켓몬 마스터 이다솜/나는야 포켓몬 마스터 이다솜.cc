82bc60ee50cbc9a406239fc7115df03423e9edbf
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-08-14 00:04:31 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 228 ms, Memory: 23944 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	map<string,int> Pokemon;
+	map<int,string> PokemonNum;
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 1; i <= N + M; i++) 
+	{
+
+		if (i <= N)
+		{
+			string str;
+			cin >> str;
+			Pokemon.insert(pair<string, int>(str, i));
+			PokemonNum.insert(pair<int,string>(i,str));
+
+		}
+		else
+		{
+			string find;
+			cin >> find;
+
+			if (!isdigit(find[0]))
+			{
+				cout<<Pokemon[find]<<"\n";
+			}
+			else
+				cout << PokemonNum[stoi(find)] << "\n";
+
+		}
+
+	}
+
+}
+
