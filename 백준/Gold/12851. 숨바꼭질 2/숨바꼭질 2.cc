b5734208c7b530b25d70ddf286b5fe523f9450de
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-08-29 17:12:01 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 28 ms, Memory: 5592 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+bool vis[100001];
+int n, k,cnt;
+
+int bfs(int start)
+{
+	queue<pair<int,int>> q;
+	q.push({ start,0 });
+    
+	int ansTime = 0;
+	while (!q.empty())
+	{
+		
+		int cur = q.front().first;
+		int time = q.front().second;
+		q.pop();
+		vis[cur] = true;
+
+		int back = cur - 1;
+		int front = cur + 1;
+		int jump = 2 * cur;
+
+		if (k == cur&&cnt==0)
+		{
+		
+			cnt++;
+			ansTime = time;
+			continue;
+		}
+
+		if (ansTime == time&&k==cur)
+		{
+			cnt++;
+			continue;
+		}
+
+
+		if (back >= 0 && !vis[back])
+		{
+			q.push({ back,time + 1 });
+		}
+		if (front <= 100000 && !vis[front])
+		{
+			q.push({ front,time + 1 });
+		}
+		if (jump <= 100000 && !vis[jump])
+		{
+			q.push({ jump,time + 1 });
+		}
+
+	}
+	return ansTime;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> k;
+
+	int mTime = bfs(n);
+
+	cout << mTime << '\n';
+	cout << cnt;
+}
+
