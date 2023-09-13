888c54783117a27c0485cf12d887840ba971ac9f
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-09-13 16:08:49 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+using namespace std;
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n / 4; i++)
+	{
+		cout << "long ";
+	}
+	cout << "int";
+	return 0;
+}
