124520b6c4f1d7ea73391fb476632ecd8cdba284
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-09-14 14:58:15 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define INF 987654321
+typedef long long ll;
+using namespace std;
+
+int n;
+vector<pair<int, int>> v;
+priority_queue<int> pq;
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		v.push_back({ b,a });
+	}
+	sort(v.begin(), v.end());
+	int ret = 0;
+	for (int i = 0; i < n; i++)
+	{
+		ret += v[i].second;
+		pq.push(-v[i].second);
+		if (pq.size() > v[i].first)
+		{
+			ret-=-pq.top();
+			pq.pop();
+		}
+	}
+	cout << ret;
+	return 0;
+}
