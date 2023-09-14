0c36139177df6e2ccdde42be3cab1fa33c050794
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-09-14 14:57:38 +0900

    [Gold II] Title: 컵라면, Time: 56 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -19,18 +19,17 @@ int main(void)
 		v.push_back({ a,b });
 	}
 	sort(v.begin(), v.end());
+	int ret = 0;
 	for (int i = 0; i < n; i++)
 	{
+		ret += v[i].second;
 		pq.push(-v[i].second);
-		if (pq.size() > v[i].first) pq.pop();
+		if (pq.size() > v[i].first)
+		{
+			ret-=-pq.top();
+			pq.pop();
+		}
 	}
-	int ret = 0;
-	while (!pq.empty())
-	{
-		ret += -pq.top();
-		pq.pop();
-	}
-
 	cout << ret;
 	return 0;
 }
