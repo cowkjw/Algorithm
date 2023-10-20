6b19cf308a4604384e8a89675af3cfcdea8d3b5b
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-10-20 11:52:24 +0900

    [Gold V] Title: 강의실 배정, Time: 60 ms, Memory: 10932 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -2,7 +2,8 @@
 using namespace std;
 #define ll long long
 #define INF 987654321
-int n;
+int n, m;
+vector<int> graph[200001];
 vector<pair<int, int>> v;
 int main(void)
 {
@@ -19,32 +20,18 @@ int main(void)
 	sort(v.begin(), v.end());
 	priority_queue<pair<int, int>>pq;// 끝 시작
 	pq.push({ -v[0].second,v[0].first });
-	queue<pair<int, int>>q;
+	int ret = pq.size();
 	for (int i = 1; i < v.size(); i++)
 	{
-		q.push({ v[i].first,v[i].second });
-	}
-	int ret = pq.size();
-	while (!pq.empty())
-	{
-		if (!q.empty())
+		if (-pq.top().first > v[i].first)
 		{
-			if (-pq.top().first > q.front().first)
-			{
-				pq.push({ -q.front().second,q.front().first });
-				ret = pq.size();
-				q.pop();
-			}
-			else
-			{
-				pq.pop();
-				pq.push({ -q.front().second,q.front().first });
-				q.pop();
-			}
+			pq.push({ -v[i].second,v[i].first });
+			ret = pq.size();
 		}
 		else
 		{
 			pq.pop();
+			pq.push({ -v[i].second,v[i].first });
 		}
 	}
 	cout << ret;
