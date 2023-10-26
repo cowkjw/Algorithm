948cc0ee69ebbd820e1944afeb8b95c31ba4a94e
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-10-26 18:30:47 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 987654321
+#define X first 
+#define Y second
+
+using namespace std;
+int n, m;
+vector<int> v;
+vector<int> res;
+void go(int idx)
+{
+	if (m == res.size())
+	{
+		for (const auto& i : res)
+		{
+			cout << i << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = idx+1; i < n; i++)
+	{
+		res.push_back(v[i]);
+		go(i);
+		res.pop_back();
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+
+	go(-1);
+
+	return 0;
+}
+
+
