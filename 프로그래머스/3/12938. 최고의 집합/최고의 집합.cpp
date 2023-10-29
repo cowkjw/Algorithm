f887c45ee5a80fc6ec7f066d6df8028676af0b8c
JangWon <83215829+cowkjw@users.noreply.github.com>
2023-10-29 14:59:50 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int n, int s) {
+    vector<int> answer;
+    if(n>s) return {-1};
+    
+    while(s!=0)
+    {
+        int tmp = s/n;
+        if(s%n!=0)
+        {
+           tmp++;
+        }
+        answer.push_back(tmp);
+        s-=tmp;
+        n--;
+    }
+
+
+    sort(answer.begin(),answer.end());
+    
+    return answer;
+}
