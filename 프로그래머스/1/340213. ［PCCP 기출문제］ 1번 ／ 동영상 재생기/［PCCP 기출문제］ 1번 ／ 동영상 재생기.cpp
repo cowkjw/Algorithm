b4f770c1d23aab00d168f80b670abc117e402230
JangWon <83215829+cowkjw@users.noreply.github.com>
2024-09-27 22:39:47 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
@@ -0,0 +1,85 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int Check_Start_Pos(const int& pos, const int& s, const int& e)
+{
+    if(s <= pos && pos <= e)
+    {
+        return e;
+    }
+    
+    return pos;
+}
+
+string Convert_Time_To_String(int t, bool isMin)
+{
+    string tmpString("");
+    if(isMin)
+    {
+        tmpString = to_string((t/60));
+        while(tmpString.size()<2)
+        {
+            tmpString = "0" + tmpString;
+        }
+    }
+    else
+    {
+        tmpString = to_string((t%60));
+        while(tmpString.size()<2)
+        {
+            tmpString = "0" + tmpString;
+        }
+    }
+    return tmpString; 
+}
+
+string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands) {
+    string answer = "";
+    
+    int total_Video_Len = stoi(video_len.substr(0,2)) * 60 + stoi(video_len.substr(3,2));
+    int start = stoi(op_start.substr(0,2)) * 60 + stoi(op_start.substr(3,2));
+    int end = stoi(op_end.substr(0,2)) * 60 + stoi(op_end.substr(3,2));
+    int iPos = stoi(pos.substr(0,2)) * 60 + stoi(pos.substr(3,2));
+    int tmpTime = Check_Start_Pos(iPos,start,end);
+   
+    for(const auto& com : commands)
+    {
+        if(start <= tmpTime && tmpTime <= end)
+        {
+            tmpTime = end;
+        }
+        
+        if(com == "next")
+        {
+            tmpTime +=10;
+        }
+        else
+        {
+            tmpTime -=10;
+        }
+    
+        if(tmpTime <= 0) 
+        {
+            tmpTime = 0;
+        }
+        else if(tmpTime >= total_Video_Len) 
+        {
+            tmpTime = total_Video_Len;
+        }
+        else if(start <= tmpTime && tmpTime <= end)
+        {
+            tmpTime = end;
+        }
+        else if(tmpTime + 10 > total_Video_Len)
+        {
+            tmpTime = total_Video_Len;
+        }
+    }
+    string minute = Convert_Time_To_String(tmpTime,true);
+    string sec = Convert_Time_To_String(tmpTime,false);
+    answer = minute + ":" + sec; 
+    
+    return answer;
+}
