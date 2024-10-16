bf8bc9a310360f4fb1a399e07d716f4b67456f37
JangWon <83215829+cowkjw@users.noreply.github.com>
2024-10-16 14:49:21 +0900

    [Silver IV] Title: 청기 백기, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	// 제곱수
+	int ans = pow(n, 0.5);
+	cout << ans;
+	return 0;
+}
