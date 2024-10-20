5074de1edda26dc67ce4e81ad216c230e068a9cb
JangWon <83215829+cowkjw@users.noreply.github.com>
2024-10-20 20:14:38 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -1,49 +1,42 @@
-#include <bits/stdc++.h>
-#define _CRT_SECURE_NO_WARNINGS
-#define INF 987654321
-#define X first 
-#define Y second
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <string>
+#include <iterator> 
 using namespace std;
+
 int n;
-int vis[11];
-struct Food
+int ans = 1e9;
+vector<vector<int>> vec;
+
+void Recur(int idx, int sin, int ssen, int use)
 {
-	int s, b;
-};
-vector<Food> v;
-int ret = INF;
+	if (idx == n)
+	{
+		if (use == 0) return;
+		ans = min(abs(sin - ssen), ans);
+		return;
+	}
+
+	Recur(idx + 1, sin * vec[idx][0], ssen + vec[idx][1], use + 1);
+	Recur(idx + 1, sin, ssen, use);
+}
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
 
 	cin >> n;
 
 	for (int i = 0; i < n; i++)
 	{
-		int s, b;
-		cin >> s >> b;
-		v.push_back({ s,b });
-	}
+		int a, b;
+		cin >> a >> b;
 
-	for (int i = 1; i < (1 << n); i++)
-	{
-		int a = 1;
-		int b = 0;
-		for (int j = 0; j < n; j++)
-		{
-			if (i &(1<<j))
-			{
-				a *= v[j].s;
-				b += v[j].b;
-			}
-		}
-		ret = min(abs(a - b), ret);
+		vec.push_back({a,b});
 	}
-	cout << ret;
+	Recur(0, 1, 0, 0);
+	cout << ans;
 	return 0;
 }
-
-
