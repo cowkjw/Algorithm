9d7e96397df4dae7a279d03878d56adb797a9c43
JangWon <83215829+cowkjw@users.noreply.github.com>
2024-10-24 16:02:12 +0900

    [Gold V] Title: 경쟁적 전염, Time: 8 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
@@ -1,60 +1,66 @@
-#include <bits/stdc++.h>
-#define INF 987654321
-#define X first 
-#define Y second
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <set>
 
+#define INF 987654321
+#define MOD 1000000
 using namespace std;
 
-vector<pair<int, int>> del{ {1,0} ,{-1,0} ,{0,1} ,{0,-1} };
-int n, k, s, x, y;
+int n, k;
 int board[201][201];
-
-int main()
+int vis[201][201];
+int s, x, y;
+vector<pair<int, int>> delta{ {1,0},{-1,0},{0,1},{0,-1} };
+int main(void)
 {
-	ios::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	cin >> n >> k;
-
-	vector<queue<pair<int, int>>> virus(k+1);
-	for (int i = 1; i <= n; i++)
+	priority_queue<tuple<int,int, int>> pq;
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 1; j <= n; j++)
+		for (int j = 0; j < n; j++)
 		{
 			cin >> board[i][j];
-			if (board[i][j] != 0)
-			{
-				virus[board[i][j]].push({ i,j });
-			}
+			if (board[i][j]) pq.push({-board[i][j], i,j}); // 바이러스 위치를 전부 큐에 삽입
 		}
 	}
 
-	cin >> s >> x >> y;// s 시간
-
-	while (s--)
+	cin >> s >> x >> y;
+	int t = 0;
+	priority_queue<tuple<int, int, int>> tmp;
+	while (!pq.empty()&& t<s)
 	{
-		for (int i = 1; i <= k; i++)
+		tmp = pq;
+		pq = priority_queue<tuple<int, int, int>>();
+		while (!tmp.empty())
 		{
-			int size = virus[i].size();
-			while (size--)
+			int virus, Y, X;
+			tie(virus, Y, X) = tmp.top();
+			virus *= -1;
+			tmp.pop();
+
+			for (const auto& dir : delta)
 			{
-				auto cur = virus[i].front();
-				virus[i].pop();
+				int ny = Y + dir.first;
+				int nx = X + dir.second;
 
-				for (int dir = 0; dir < 4; dir++)
+				if (ny < 0 || nx < 0 || nx >= n || ny >= n) continue;
+				if (board[ny][nx] == 0)
 				{
-					int nx = del[dir].X + cur.X;
-					int ny = del[dir].Y + cur.Y;
-
-					if (nx > 0 && nx <= n && ny > 0 && ny <= n && board[nx][ny] == 0)
-					{
-						board[nx][ny] = i;
-						virus[i].push({ nx,ny });
-					}
+					board[ny][nx] = virus;
+					pq.push({ -virus,ny,nx });
 				}
 			}
 		}
+		t++;
 	}
-	cout << board[x][y];
+
+
+	cout << board[x-1][y-1];
 	return 0;
 }
