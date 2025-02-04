140d1edf174b12174cd997432bf8a5226383e72e
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-02-04 16:08:10 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,49 +1,57 @@
-#include <bits/stdc++.h>
-#define INF 987654321
-typedef long long ll;
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <tuple>
 using namespace std;
-
-vector<pair<int, int>> dir {{1, 0}, { 0,1 }, { -1,0 }, { 0,-1 }};
-
-int dist[101][101];
-int n, m;
-vector<string> board;
-
-int main(void)
+#define INF 987654321
+const int dx[4]{ 1,-1,0,0 };
+const int dy[4]{ 0,0,1,-1 };
+int dist[10][10];
+bool vis[10][10];
+int seq[10][10];
+int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
+    int n, m;
+    cin >> n >> m;
+    
+    vector<string> vec(n);
+    vector<vector<int>> dist(n, vector<int>(m,INF));
 
-	for (int i = 0; i < n; i++)
-	{
-		string str;
-		cin >> str;
-		board.push_back(str);
-	}
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vec[i];
+    }
 
-	queue<pair<int, int>> q;
-	q.push({0,0});
-	dist[0][0] = 1;
-	while (!q.empty())
-	{
-		auto cur = q.front();
-		q.pop();
-		for (const auto di : dir)
-		{
-			int nx = cur.first + di.first;
-			int ny = cur.second + di.second;
 
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-			if (dist[nx][ny]) continue;
-			if (board[nx][ny]=='1')
-			{
-				dist[nx][ny] = dist[cur.first][cur.second] + 1;
-				q.push({ nx,ny });
-			}
-		}
-	}
-	cout << dist[n-1][m-1];
-	return 0;
+    queue<pair<int, int>> q;
+
+    q.push({ 0,0 });
+    dist[0][0] = 1;
+
+    while (!q.empty())
+    {
+        int x, y;
+        tie(x, y) = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+            int nCost = dist[x][y] + 1;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m || vec[nx][ny] == '0') continue;
+
+            if (nCost < dist[nx][ny])
+            {
+                dist[nx][ny] = nCost;
+                q.push({ nx,ny });
+            }
+        }
+    }
+
+    cout << dist[n-1][m-1];
 }
