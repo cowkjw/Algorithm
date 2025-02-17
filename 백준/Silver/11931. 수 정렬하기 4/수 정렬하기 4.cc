bf97f03dd492df491d23e2314419a8f5e0139500
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-02-17 15:41:45 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 596 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <set>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+#define INF 987654321
+const int dx[4]{ 1,-1,0,0 };
+const int dy[4]{ 0,0,1,-1 };
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> vec(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i];
+	}
+	sort(vec.rbegin(), vec.rend());
+	for (int i : vec)
+	{
+		cout << i << '\n';
+	}
+	return 0;
+}
