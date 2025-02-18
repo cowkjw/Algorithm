d08b50e284ba22b8868a7077612ea00223696660
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-02-18 15:19:55 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+#define INF 987654321
+const int dx[4]{ 1,-1,0,0 };
+const int dy[4]{ 0,0,1,-1 };
+
+unordered_map<string, string > um;
+int main()
+{
+	long long n, m;
+	cin >> n >> m;
+	cout << abs(n - m);
+	return 0;
+}
