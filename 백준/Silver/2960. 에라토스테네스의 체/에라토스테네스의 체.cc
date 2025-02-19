fbb2b7e05366409da389ad9bf20c6640bf061797
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-02-19 19:54:28 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+#define INF 987654321
+const int dx[4]{ 1,-1,0,0 };
+const int dy[4]{ 0,0,1,-1 };
+
+int main()
+{
+	int n, m;
+	cin >> n>>m;
+	vector<int>vec(n+1);
+	for (int i = 2; i <= n; i++)
+	{
+		vec[i] = n;
+	}
+
+	int cnt = 0;
+	for (int i = 2; i<= n; i ++)
+	{
+		if (vec[i] == 0) continue;
+		vec[i] = 0;
+		cnt++;
+		if (cnt == m)
+		{
+			cout << i;
+			return 0;
+		}
+		for (int j = 2*i; j <= n; j += i)
+		{
+			if (vec[j] == 0) continue;
+			vec[j] = 0;
+			cnt++;
+			if (cnt == m)
+			{
+				cout << j;
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
