fb26bb674ec8e610a648c7a22a9030a41e8aaac9
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-10-26 16:37:54 +0900

    [Silver V] Title: 카드1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include<deque>
+#include <list>
+#include<numeric>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <unordered_set>
+#include <functional>
+#include <set>
+#include <map>
+#include <sstream>
+using namespace std;
+#define INF 987654321
+#define LL_INF 1e15
+
+
+using ll = long long;
+using pii = pair<int, int>;
+
+int n, m, k;
+
+const int dx[] = { -1,1,0,0 };
+const int dy[] = { 0,0,-1,1 };
+const int hDx[] = { -2,-2,-1,-1,1,1,2,2 };
+const int hDy[] = { -1,1,-2,2,-2,2,-1,1 };
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+	queue<int> q;
+	for (int i = 0; i < n; i++)
+	{
+		q.push(i + 1);
+	}
+
+	while (!q.empty())
+	{
+		cout << q.front() << " ";
+		q.pop();
+		if (!q.empty())
+		{
+			q.push(q.front());
+			q.pop();
+		}
+	}
+	return 0;
+}
