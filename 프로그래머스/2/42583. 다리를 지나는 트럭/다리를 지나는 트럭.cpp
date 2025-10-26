d3add8080abeafdb80acd5befdc9d32c6695b92a
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-10-26 18:47:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.37 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -6,31 +6,31 @@ using namespace std;
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
     queue<int> q;
-    for (int i = 0; i < bridge_length; i++) q.push(-1);
-
-    int cur = 0;
-    int i = 0;
-    while (1)
+    int idx = 0;
+    int sum = 0;
+    while(true)
     {
-        if (q.front() != -1) cur -= q.front();
-         if (cur + truck_weights[i] <= weight)
+        answer++;
+        if(q.size() == bridge_length)
         {
-            q.push(truck_weights[i]);
-            cur += truck_weights[i];
-            i++;
+            sum-=q.front();
+            q.pop();
+        }
+        
+        if(idx<truck_weights.size()&& sum + truck_weights[idx]<=weight)
+        {
+            q.push(truck_weights[idx]);
+            sum+=truck_weights[idx++];
         }
         else
         {
-            q.push(-1);
+            q.push(0);
         }
-        answer++;
-        q.pop();
-        if (i >= truck_weights.size()) break;
-    }
-    while (!q.empty())
-    {
-        answer++;
-        q.pop();
+        
+        if(idx == truck_weights.size()&& sum == 0)
+        {
+            break;
+        }
     }
     return answer;
 }
