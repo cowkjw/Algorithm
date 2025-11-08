511820c557f418aa1575f2500806ad9b466a0f22
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-11-08 23:45:49 +0900

    [Silver III] Title: 선물, Time: 0 ms, Memory: 2468 KB -BaekjoonHub

---
+++ 백준/Silver/1166. 선물/선물.cc
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include<deque>
+#include <list>
+#include<numeric>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <unordered_set>
+#include <functional>
+#include <set>
+#include <map>
+#include <sstream>
+using namespace std;
+#define INF 987654321
+#define LL_INF 1e15
+
+
+using ll = long long;
+using pii = pair<int, int>;
+
+int n, m, k;
+
+const int dx[] = { -1,1,0,0 };
+const int dy[] = { 0,0,-1,1 };
+const int hDx[] = { -2,-2,-1,-1,1,1,2,2 };
+const int hDy[] = { -1,1,-2,2,-2,2,-1,1 };
+
+int parent[100001];
+int Find(int x)
+{
+	if (parent[x] == x) return x;
+	return parent[x] = Find(parent[x]);
+}
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+	if (a < b) parent[b] = a;
+	else parent[a] = b;
+}
+
+// 문자열 분리 (split)
+vector<string> split(string s, char delim)
+{
+	vector<string> result;
+	stringstream ss(s);
+	string item;
+
+	while (getline(ss, item, delim))
+	{
+		if (!item.empty())
+		{  // 빈 문자열 제외
+			result.push_back(item);
+		}
+	}
+
+	return result;
+}
+string trim(string s)
+{
+	if (s.empty()) return s;
+
+	int start = 0, end = s.length() - 1;
+	while (start <= end && isspace(s[start])) start++;
+	while (end >= start && isspace(s[end])) end--;
+
+	if (start > end) return "";
+	return s.substr(start, end - start + 1);
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll w, h,l;
+	cin >> n >> l >> w >> h;
+
+	double s = 0, e = max({ l,w,h });
+
+	for (int i = 0; i < 1000; ++i)
+	{
+				double mid = (s + e) / 2;
+
+		ll cnt = (ll)(l / mid) * (ll)(w / mid) * (ll)(h / mid);
+
+		if (cnt >= n)
+		{
+			s = mid;
+		}
+		else
+		{
+			e = mid;
+		}
+	}
+	cout.setf(ios::fixed);
+	cout.precision(10);
+	cout << s;
+	return 0;
+}
