a98a42e7eefb456a38288e0709d872b275759fab
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-11-17 00:07:15 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 96 ms, Memory: 24016 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -11,29 +11,95 @@
 #include <functional>
 #include <set>
 #include <map>
+#include <sstream>
 using namespace std;
+#define INF 987654321
+#define LL_INF 1e15
 
+
+using ll = long long;
+using pii = pair<int, int>;
+
+int n, m, k;
+
+const int dx[] = { -1,1,0,0 };
+const int dy[] = { 0,0,-1,1 };
+const int hDx[] = { -2,-2,-1,-1,1,1,2,2 };
+const int hDy[] = { -1,1,-2,2,-2,2,-1,1 };
+
+int parent[100001];
+int Find(int x)
+{
+	if (parent[x] == x) return x;
+	return parent[x] = Find(parent[x]);
+}
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+	if (a < b) parent[b] = a;
+	else parent[a] = b;
+}
+
+// 문자열 분리 (split)
+vector<string> split(string s, char delim)
+{
+	vector<string> result;
+	stringstream ss(s);
+	string item;
+
+	while (getline(ss, item, delim))
+	{
+		if (!item.empty())
+		{  // 빈 문자열 제외
+			result.push_back(item);
+		}
+	}
+
+	return result;
+}
+string trim(string s)
+{
+	if (s.empty()) return s;
+
+	int start = 0, end = s.length() - 1;
+	while (start <= end && isspace(s[start])) start++;
+	while (end >= start && isspace(s[end])) end--;
+
+	if (start > end) return "";
+	return s.substr(start, end - start + 1);
+}
+
+
+int dp[1001];
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-
-
+	cout.tie(0);
 	string str;
 	cin >> str;
-	int n;
+	vector<vector<int>> prefix(26, vector<int>(200001, 0));
+
+	for (int i = 1;i<= str.size();i++)
+	{
+		int cur = str[i - 1] - 'a';
+
+		for (int c = 0; c < 26; c++)
+			prefix[c][i] = prefix[c][i - 1];
+
+		prefix[cur][i]++;
+	}
+	
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
 		char ch;
-		int start, end;
-		cin >> ch >> start >> end;
-
-		int ans = 0;
-		for (int j = start; j <= end; j++)
-		{
-			if (ch == str[j]) ans++;
-		}
+		int s, e;
+		cin >> ch>>s>>e;
+		int pos = ch-'a';
+		int ans = prefix[pos][e + 1] - prefix[pos][s];
 		cout << ans << '\n';
 	}
+	return 0;
 }
