5ee665f282fc5c42fc0481bdfcd4d3c445c9cd74
JangWon <83215829+cowkjw@users.noreply.github.com>
2025-11-17 11:23:39 +0900

    [Gold V] Title: 조 짜기, Time: 0 ms, Memory: 2472 KB -BaekjoonHub

---
+++ 백준/Gold/2229. 조 짜기/조 짜기.cc
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include<deque>
+#include <list>
+#include<numeric>
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <unordered_set>
+#include <functional>
+#include <set>
+#include <map>
+#include <sstream>
+using namespace std;
+#define INF 987654321
+#define LL_INF 1e15
+
+
+using ll = long long;
+using pii = pair<int, int>;
+
+int n, m, k;
+
+const int dx[] = { -1,1,0,0 };
+const int dy[] = { 0,0,-1,1 };
+const int hDx[] = { -2,-2,-1,-1,1,1,2,2 };
+const int hDy[] = { -1,1,-2,2,-2,2,-1,1 };
+
+int parent[100001];
+int Find(int x)
+{
+	if (parent[x] == x) return x;
+	return parent[x] = Find(parent[x]);
+}
+void Union(int a, int b)
+{
+	a = Find(a);
+	b = Find(b);
+	if (a < b) parent[b] = a;
+	else parent[a] = b;
+}
+
+// 문자열 분리 (split)
+vector<string> split(string s, char delim)
+{
+	vector<string> result;
+	stringstream ss(s);
+	string item;
+
+	while (getline(ss, item, delim))
+	{
+		if (!item.empty())
+		{  // 빈 문자열 제외
+			result.push_back(item);
+		}
+	}
+
+	return result;
+}
+string trim(string s)
+{
+	if (s.empty()) return s;
+
+	int start = 0, end = s.length() - 1;
+	while (start <= end && isspace(s[start])) start++;
+	while (end >= start && isspace(s[end])) end--;
+
+	if (start > end) return "";
+	return s.substr(start, end - start + 1);
+}
+
+
+int dp[1001];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+
+	vector<int> arr(n+1);
+
+	for (int i = 1; i <= n; i++) cin >> arr[i];
+
+	for (int i = 1; i <= n; i++)
+	{
+		int minS = arr[i];
+		int maxS = arr[i];
+		dp[i] = dp[i - 1];
+		for (int j = i; j >= 1; j--)
+		{
+			minS = min(minS, arr[j]);
+			maxS = max(maxS, arr[j]);
+			dp[i] = max(dp[i], dp[j - 1] + (maxS - minS));
+		}
+	}
+	cout << dp[n];
+	return 0;
+}
